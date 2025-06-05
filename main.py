5ad025e563dad9ace5f72605ab4e6df3720da585
danielsmit1998 <danielsmit98@gmail.com>
2025-06-05 16:49:14 +0200

    Added groups to pygame updating and drawing.
    The player can move now.

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(x = SCREEN_WIDTH/2, y = SCREEN_HEIGHT/2)
+    
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -16,7 +22,16 @@ def main():
                 return
     
         screen.fill((0,0,0))
+
+        for player in drawable:
+            player.draw(screen)
+
+        updateable.update(dt)
+
+        # hier wordt het scherm geupdate
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
